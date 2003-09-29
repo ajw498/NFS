928ac7f074c285d8a7486e4ddc53156443388156
Alex Waugh <alex@alexwaugh.com>
2003-09-29 22:52:05 +0000

    Tidying up and commenting

--- imageentry_func.c
+++ imageentry_func.c
@@ -27,6 +27,41 @@
 /*FIXME*/
 #define IMAGEERRBASE 20
 
+
+#define NFSSTATBASE 1
+/*FIXME*/
+
+#define MAX_PAYLOAD 7000
+/*FIXME*/
+
+
+/* Generate a RISC OS error block based on the NFS status code */
+static os_error *gen_nfsstatus_error(enum nstat stat)
+{
+	char *str;
+
+	switch (stat) {
+		case NFSERR_PERM: str = "Not owner"; break;
+		case NFSERR_NOENT: str = "No such file or directory"; break;
+		case NFSERR_IO: str = "IO error"; break;
+		case NFSERR_NXIO: str = "No such device or address"; break;
+		case NFSERR_ACCES: str = "Permission denied"; break;
+		case NFSERR_EXIST: str = "File already exists"; break;
+		case NFSERR_NODEV: str = "No such device"; break;
+		case NFSERR_NOTDIR: str = "Not a directory"; break;
+		case NFSERR_ISDIR: str = "Is a directory"; break;
+		case NFSERR_FBIG: str = "File too large"; break;
+		case NFSERR_NOSPC: str = "No space left on device"; break;
+		case NFSERR_ROFS: str = "Read only filesystem"; break;
+		case NFSERR_NAMETOOLONG: str = "File name too long"; break;
+		case NFSERR_NOTEMPTY: str = "Directory not empty"; break;
+		case NFSERR_DQUOT: str = "Disc quota exceeded"; break;
+		case NFSERR_STALE: str = "Stale NFS filehandle"; break;
+		default: str = "Unknown error"; break;
+	}
+	return gen_error(NFSSTATBASE, "NFS call failed (%s)", str);
+}
+
 static void free_conn_info(struct conn_info *conn)
 {
 	if (conn == NULL) return;
@@ -291,8 +326,8 @@ os_error *func_newimage(unsigned int fileswitchhandle, struct conn_info **myhand
 	}
 	if (rootfh.status != 0) {
 		free_conn_info(conn);
-		/* status is an errno value, not exactly portable */
-		return gen_error(IMAGEERRBASE + 0, "Mount of '%s' failed (%d)", conn->export, rootfh.status);
+		/* status is an errno value. Probably the same as an NFS status value. */
+		return gen_nfsstatus_error((enum nstat)rootfh.status);
 	}
 	memcpy(conn->rootfh, rootfh.u.directory, FHSIZE);
 
@@ -301,11 +336,11 @@ os_error *func_newimage(unsigned int fileswitchhandle, struct conn_info **myhand
 
 
 struct dir_entry {
-	int load;
-	int exec;
-	int len;
-	int attr;
-	int type;
+	unsigned int load;
+	unsigned int exec;
+	unsigned int len;
+	unsigned int attr;
+	unsigned int type;
 };
 
 #define return_error(msg) return gen_error(1,msg)
@@ -394,7 +429,12 @@ static int filename_riscosify(char *name, int len, char **buffer)
 	return filetype;
 }
 
-static os_error *lookup_leafname(char *dhandle, char *leafname, int len,  struct opaque **found, struct conn_info *conn)
+/* Find leafname,xyz given leafname by enumerating the entire directory
+   until a matching file is found. If there are two matching files, the
+   first found is used. This function would benefit in many cases by some
+   cacheing of leafnames and extensions. The returned leafname is a pointer
+   to static data and should be copied before any subsequent calls. */
+static os_error *lookup_leafname(char *dhandle, struct opaque *leafname, struct opaque **found, struct conn_info *conn)
 {
 	static struct readdirargs rddir;
 	static struct readdirres rdres;
@@ -402,22 +442,24 @@ static os_error *lookup_leafname(char *dhandle, char *leafname, int len,  struct
 	os_error *err;
 
 	memcpy(rddir.dir, dhandle, FHSIZE);
-	rddir.count = 1024; /**/
+	rddir.count = MAX_PAYLOAD;
 	rddir.cookie = 0;
+
 	do {
 		err = NFSPROC_READDIR(&rddir, &rdres, conn);
 		if (err) return err;
-		if (rdres.status != NFS_OK) return_error("readdir failed");
+		if (rdres.status != NFS_OK) return gen_nfsstatus_error(rdres.status);
+
 		direntry = rdres.u.readdirok.entries;
 		while (direntry) {
 			rddir.cookie = direntry->cookie;
-			if (direntry->name.size == len || direntry->name.size == len + 4) {
-				if (strncmp(direntry->name.data, leafname, len) == 0) {
-					if ((direntry->name.size == len)
-					    || (direntry->name.data[len] == ','
-					        && isxdigit(direntry->name.data[len+1])
-					        && isxdigit(direntry->name.data[len+2])
-					        && isxdigit(direntry->name.data[len+3]))) {
+
+			if (direntry->name.size == leafname->size + sizeof(",xyz") - 1) {
+				if (strncmp(direntry->name.data, leafname->data, leafname->size) == 0) {
+					if (direntry->name.data[leafname->size] == ','
+					     && isxdigit(direntry->name.data[leafname->size + 1])
+					     && isxdigit(direntry->name.data[leafname->size + 2])
+					     && isxdigit(direntry->name.data[leafname->size + 3])) {
 						*found = &(direntry->name);
 						return NULL;
 					}
@@ -426,39 +468,17 @@ static os_error *lookup_leafname(char *dhandle, char *leafname, int len,  struct
 			direntry = direntry->next;
 		}
 	} while (!rdres.u.readdirok.eof);
+
 	*found = NULL;
 	return NULL;
 }
 
-#define NFSSTATBASE 1
-/*FIXME*/
-
-static os_error *gen_nfsstatus_error(enum nstat stat)
-{
-	char *str;
-
-	switch (stat) {
-		case NFSERR_PERM: str = "Not owner"; break;
-		case NFSERR_NOENT: str = "No such file or directory"; break;
-		case NFSERR_IO: str = "IO error"; break;
-		case NFSERR_NXIO: str = "No such device or address"; break;
-		case NFSERR_ACCES: str = "Permission denied"; break;
-		case NFSERR_EXIST: str = "File already exists"; break;
-		case NFSERR_NODEV: str = "No such device"; break;
-		case NFSERR_NOTDIR: str = "Not a directory"; break;
-		case NFSERR_ISDIR: str = "Is a directory"; break;
-		case NFSERR_FBIG: str = "File too large"; break;
-		case NFSERR_NOSPC: str = "No space left on device"; break;
-		case NFSERR_ROFS: str = "Read only filesystem"; break;
-		case NFSERR_NAMETOOLONG: str = "File name too long"; break;
-		case NFSERR_NOTEMPTY: str = "Directory not empty"; break;
-		case NFSERR_DQUOT: str = "Disc quota exceeded"; break;
-		case NFSERR_STALE: str = "Stale nfs filehandle"; break;
-		default: str = "Unknown error"; break;
-	}
-	return gen_error(NFSSTATBASE, "NFS call failed (%s)", str);
-}
-
+/* Convert a full filename/dirname into a leafname, and find the nfs handle
+   for the file/dir and for the directory containing it.
+   Returns a pointer to static data.
+   This function would really benefit from some cacheing.
+   In theory it should deal with wildcarded names, but that is just too much
+   hassle. And, really, that should be the job of fileswitch. */
 static os_error *filename_to_finfo(char *filename, struct diropok **dinfo, struct diropok **finfo, char **leafname, struct conn_info *conn)
 {
 	char *start = filename;
@@ -468,77 +488,117 @@ static os_error *filename_to_finfo(char *filename, struct diropok **dinfo, struc
 	static struct diropres next;
 	os_error *err;
 
-	if (dinfo) *dinfo = NULL;
-	if (finfo) *finfo = NULL;
+	if (dinfo) *dinfo = NULL; /* A NULL directory indicates the root directory */
+	if (finfo) *finfo = NULL; /* A NULL file indicates it wasn't found */
 
 	do {
 		end = start;
-		/* Find end of dirname */
+		/* Find end of dirname segment */
 		while (*end && *end != '.') end++;
 
 		current.name.data = filename_unixify(start, end - start);
 		current.name.size = end - start;
+
+		if (leafname) *leafname = current.name.data;
+
 		memcpy(current.dir, dirhandle, FHSIZE);
 		err = NFSPROC_LOOKUP(&current, &next, conn);
 		if (err) return err;
 
-		if (leafname) *leafname = current.name.data;
-		if (next.status == NFSERR_NOENT) break;
+		/* It is not an error if the file isn't found, but the
+		   containing directory must exist */
+		if (*end == '\0' && next.status == NFSERR_NOENT) break;
 		if (next.status != NFS_OK) return gen_nfsstatus_error(next.status);
+
 		if (*end != '\0') {
-			if (next.u.diropok.attributes.type != NFDIR) return_error("not a directory");
+			if (next.u.diropok.attributes.type != NFDIR) {
+				/* Every segment except the leafname must be a directory */
+				return gen_nfsstatus_error(NFSERR_NOTDIR);
+			}
 			if (dinfo) *dinfo = &(next.u.diropok);
 		}
+
 		dirhandle = next.u.diropok.file;
 		start = end + 1;
 	} while (*end != '\0');
+
+	/* By this point, the directory has been found, the file may or
+	   may not have been found (either because it doesn't exist, or
+	   because we need to add an ,xyz extension) */
+
+	/* There's no need to continue if the caller doesn't care about the file */
+	if (finfo == NULL) return NULL;
+
 	if (next.status == NFSERR_NOENT) {
-		if (*end == '\0' && finfo) {
+		if (1/*conn->xyz_ext != NEVER*/) { /*FIXME */
 			struct opaque *file;
-			err = lookup_leafname(dirhandle, current.name.data, current.name.size, &file, conn);
+	
+			err = lookup_leafname(dirhandle, &(current.name), &file, conn);
 			if (err) return err;
+	
 			if (file) {
+				/* A matching file,xyz was found */
 				current.name.data = file->data;
 				current.name.size = file->size;
+	
+				/* Lookup the handle with the new leafname */
 				err = NFSPROC_LOOKUP(&current, &next, conn);
 				if (err) return err;
 				if (next.status != NFS_OK) return gen_nfsstatus_error(next.status);
+	
 				*finfo = &(next.u.diropok);
 			}
 		}
 	} else {
-		if (finfo) *finfo = &(next.u.diropok);
+		/* The file was found without needing to add ,xyz */
+		*finfo = &(next.u.diropok);
 	}
 	return NULL;
 }
 
-static void timeval_to_loadexec(struct ntimeval *unixtime, int filetype, int *load, int *exec)
+/* Convert a unix timestamp into a RISC OS load and execution address */
+static void timeval_to_loadexec(struct ntimeval *unixtime, int filetype, unsigned int *load, unsigned int *exec)
 {
-	int64_t csecs;
+	uint64_t csecs;
+
 	csecs = unixtime->seconds;
 	csecs *= 100;
-	csecs += (int64_t)(unixtime->useconds / (long)10);
+	csecs += ((int64_t)unixtime->useconds / 10000);
 	csecs += 0x336e996a00; /* Difference between 1900 and 1970 */
 	*load = (int)((csecs >> 32) & 0xFF);
 	*load |= (0xFFF00000 | ((filetype & 0xFFF) << 8));
 	*exec = (int)(csecs & 0xFFFFFFFF);
 }
 
-static int mode_to_attr(unsigned int mode)
+/* Convert a RISC OS load and execution address into a unix timestamp */
+static void loadexec_to_timeval(unsigned int load, unsigned int exec, struct ntimeval *unixtime)
 {
-	int attr;
+	uint64_t csecs;
+
+	csecs = exec;
+	csecs |= ((uint64_t)load & 0xFF) << 32;
+	csecs -= 0x336e996a00; /* Difference between 1900 and 1970 */
+    unixtime->seconds = (unsigned int)(csecs / 100);
+    unixtime->useconds = (unsigned int)((csecs % 100) * 10000);
+}
+
+/* Convert a unix mode to RISC OS attributes */
+static unsigned int mode_to_attr(unsigned int mode)
+{
+	unsigned int attr;
 	attr  = ( mode & 0x100) >> 8; /* Owner read */
 	attr |= ( mode & 0x080) >> 6; /* Owner write */
-	attr |= (~mode & 0x080) >> 4; /* Locked */
 	attr |= ( mode & 0x004) << 2; /* Others read */
 	attr |= ( mode & 0x002) << 4; /* Others write */
 	return attr;
 }
+
+/* Read directory entries and possibly info */
 os_error *func_readdirinfo(int info, char *dirname, void *buffer, int numobjs, int start, int buflen, struct conn_info *conn, int *objsread, int *continuepos)
 {
 	char *bufferpos;
-	static struct readdirargs rddir;
-	static struct readdirres rdres;
+	struct readdirargs rddir;
+	struct readdirres rdres;
 	struct entry *direntry;
 	os_error *err;
 
@@ -546,22 +606,31 @@ os_error *func_readdirinfo(int info, char *dirname, void *buffer, int numobjs, i
 	*objsread = 0;
 
 	if (dirname[0] == '\0') {
+		/* An empty dirname specifies the root directory */
 		memcpy(rddir.dir, conn->rootfh, FHSIZE);
 	} else {
 		struct diropok *finfo;
+
+		/* Find the handle of the directory */
 		err = filename_to_finfo(dirname, NULL, &finfo, NULL, conn);
 		if (err) return err;
-		if (finfo == NULL) return_error("dir doesn't exist");
+		if (finfo == NULL) return gen_nfsstatus_error(NFSERR_NOENT);
+
 		memcpy(rddir.dir, finfo->file, FHSIZE);
 	}
-	rddir.count = 1024;
+
+	rddir.count = MAX_PAYLOAD;
 	rddir.cookie = start;
 	err = NFSPROC_READDIR(&rddir, &rdres, conn);
 	if (err) return err;
 	if (rdres.status != NFS_OK) return gen_nfsstatus_error(rdres.status);
+
+	/* We may need to do a lookup on each filename, but this would
+	   corrupt the rx buffer which holds our list, so swap buffers */
 	swap_rxbuffers();
+
 	direntry = rdres.u.readdirok.entries;
-	while (direntry) {
+	while (direntry && *objsread < numobjs) {
 		if (direntry->name.size == 0) {
 			/* Ignore null names */
 		} else if (!conn->hidden && direntry->name.data[0] == '.') {
@@ -571,40 +640,57 @@ os_error *func_readdirinfo(int info, char *dirname, void *buffer, int numobjs, i
 		} else if (direntry->name.size == 2 && direntry->name.data[0] == '.' && direntry->name.data[1] == '.') {
 			/* parent dir */
 		} else {
-			struct dir_entry *entry = NULL;
+			struct dir_entry *info_entry = NULL;
 			int filetype;
 
-			/* Check buffer size */
 			if (info) {
-				entry = (struct dir_entry *)bufferpos;
+				info_entry = (struct dir_entry *)bufferpos;
 				bufferpos += sizeof(struct dir_entry);
 			}
-			filetype = filename_riscosify(direntry->name.data,direntry->name.size,&bufferpos);
+
+			/* Check there is room in the output buffer.
+			   This check may be slightly pessimistic. */
+			if (bufferpos + direntry->name.size + 4 > (char *)buffer + buflen) break;
+
+			/* Copy leafname into output buffer, translating some
+			   chars and stripping any ,xyz */
+			filetype = filename_riscosify(direntry->name.data, direntry->name.size, &bufferpos);
+
 			if (info) {
 				struct diropargs lookupargs;
 				struct diropres lookupres;
 
+				/* Lookup file attributes.
+				   READDIRPLUS in NFS3 would eliminate this call. */
 				memcpy(lookupargs.dir, rddir.dir, FHSIZE);
 				lookupargs.name.data = direntry->name.data;
 				lookupargs.name.size = direntry->name.size;
+
 				err = NFSPROC_LOOKUP(&lookupargs, &lookupres, conn);
 				if (err) return err;
 				if (lookupres.status != NFS_OK) return gen_nfsstatus_error(lookupres.status);
 
-				timeval_to_loadexec(&(lookupres.u.diropok.attributes.mtime), filetype, &(entry->load), &(entry->exec));
-				entry->len = lookupres.u.diropok.attributes.size;
-				entry->attr = mode_to_attr(lookupres.u.diropok.attributes.mode);
-				entry->type = lookupres.u.diropok.attributes.type == NFDIR ? OBJ_DIR : OBJ_FILE;
+				timeval_to_loadexec(&(lookupres.u.diropok.attributes.mtime), filetype, &(info_entry->load), &(info_entry->exec));
+				info_entry->len = lookupres.u.diropok.attributes.size;
+				info_entry->attr = mode_to_attr(lookupres.u.diropok.attributes.mode);
+				info_entry->type = lookupres.u.diropok.attributes.type == NFDIR ? OBJ_DIR : OBJ_FILE;
 			}
+
 			(*objsread)++;
 		}
 		start = direntry->cookie;
 		direntry = direntry->next;
 	}
-	if (rdres.u.readdirok.eof) {
+
+	if (direntry == NULL && rdres.u.readdirok.eof) {
 		*continuepos = -1;
+		/* If there are entries still in the buffer that won't fit in the
+		   output buffer then we should cache them to save rerequesting
+		   them on the next iteration */
 	} else {
 		*continuepos = start;
+		/* At this point we could speculativly request the next set of
+		   entries to reduce the latency on the next request */
 	}
 	return NULL;
 }
@@ -669,9 +755,6 @@ os_error *close_file(struct file_handle *handle, int load, int exec)
 	free(handle);
 	return NULL;
 }
-
-#define MAX_PAYLOAD 7000
-/*FIXME*/
 
 
 os_error *get_bytes(struct file_handle *handle, char *buffer, unsigned len, unsigned offset)
